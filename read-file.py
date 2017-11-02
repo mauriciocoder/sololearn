3ffbafab35c93f3076a72ac0b4e09cc19946ff3d
Mauricio Bonetti <bonetti.mauricio@gmail.com>
2017-11-02 14:21:20 +0000

    Read files tutorial

---
+++ read-file.py
@@ -0,0 +1,34 @@
+filename = 'test-files/file-test'
+file = open(filename, 'r')
+content = file.read()
+file.close()
+print('content = {}'.format(content))
+
+## print only part in bytes of file
+file = open(filename, 'r')
+content_of_20_bytes = file.read(20)
+print(content_of_20_bytes)
+content_of_20_bytes = file.read(20)
+print(content_of_20_bytes)
+file.close()
+
+## once a file have already been read and not closed, it wont bring any content at all!
+file = open(filename)
+content = file.read()
+print(content)
+content = file.read()
+assert len(content) == 0
+file.close()
+
+## we can also get all lines of file
+file = open(filename, 'r')
+lines = file.readlines()
+print(lines)
+file.close()
+
+## or you can iterate on array
+file = open(filename, 'r')
+for line in file:
+    print(line.strip())
+file.close()
+
